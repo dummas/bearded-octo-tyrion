5b1da9bcee1541130db167cf5474f244cc45c4e4
Maksim Norkin <m.norkin@gmail.com>
2013-07-10 21:04:01 +0300

    Database update

--- brokenwing/settings.py
+++ brokenwing/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
     'default': {
         # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bw-planner',                      # Or path to database file if using sqlite3.
+        'NAME': 'bw_planner',                      # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
         'PASSWORD': 'sWAgu4e7',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
